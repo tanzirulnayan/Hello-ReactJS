a1a600cb228e2d3ee1359eb234e310e8b32a06aa
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2020-02-12 11:37:18 +0600

    Update App.js
    prop list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Ninjas from "./Ninjas";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> My First React App!</h1>
-      <p>Welcome :)</p>
-        <Ninjas name="Nayan" age="23" belt="Black"/>
-        <Ninjas name="Paola" age="20" belt="Black"/>
-    </div>
-  );
+class App extends Component {
+    state = {
+        ninjas: [
+            {name: 'Nayan', age: 23, belt: 'Black', id: 1},
+            {name: 'Paola', age: 20, belt: 'Pink', id: 2},
+            {name: 'Alissa', age: 19, belt: 'Green', id: 3}
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1> My First React App!</h1>
+                <p>Welcome :)</p>
+                <Ninjas ninjas={ this.state.ninjas }/>
+            </div>
+        );
+    }
 }
 
 export default App;
