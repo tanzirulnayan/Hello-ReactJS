1fa5b9ca7c69f79f64a24634dffee8976c4816dd
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2020-02-12 10:54:05 +0600

    Create Ninjas.js

---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Ninjas extends  Component{
+    render() {
+        return(
+            <div className="ninja">
+                <div>Name: Nayan</div>
+                <div>Age: 23</div>
+                <div>Belt: Black</div>
+            </div>
+        );
+    }
+}
+
+export default Ninjas;
